6fae712a9f90c9d514f8380f37c6e762c8d58c90
Morgan Karl Geldenhuys <morgan.geldenhuys@tu-berlin.de>
2021-01-22 12:11:44 +0100

    init

---
+++ failure_injector/src/main/java/de/tu_berlin/dos/arm/khaos/failure_injector/Run.java
@@ -0,0 +1,8 @@
+package de.tu_berlin.dos.arm.khaos.failure_injector;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ generator/src/main/java/de/tu_berlin/dos/arm/khaos/generator/Run.java
@@ -0,0 +1,11 @@
+package de.tu_berlin.dos.arm.khaos.generator;
+
+import de.tu_berlin.dos.arm.khaos.common.utils.FileReader;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+        System.out.println(FileReader.class);
+    }
+}
