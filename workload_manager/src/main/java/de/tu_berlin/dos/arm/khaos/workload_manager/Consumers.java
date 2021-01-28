a9f9b47e65970176aad81c8be4df2016c5df313c
Morgan Karl Geldenhuys <morgan.geldenhuys@tu-berlin.de>
2021-01-28 14:38:47 +0100

    add kafka to ffile consumer

---
+++ workload_manager/src/main/java/de/tu_berlin/dos/arm/khaos/workload_manager/Consumers.java
@@ -0,0 +1,78 @@
+package de.tu_berlin.dos.arm.khaos.workload_manager;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
+public interface Consumers {
+
+    interface Consumer {
+
+        void execute() throws Exception;
+    }
+
+    class KafkaToFileConsumer implements Consumer {
+
+        private final Properties props;
+        private final String topic;
+        private final int linesToFlush;
+        private final File output;
+
+        public KafkaToFileConsumer(String brokerList, String topic, int linesToFlush, File output) {
+
+            // Kafka Consumer Properties
+            this.props = new Properties();
+            props.put("bootstrap.servers", brokerList);
+            props.put("group.id", UUID.randomUUID().toString());
+            props.put("enable.auto.commit", "true");
+            props.put("auto.commit.interval.ms", "1000");
+            props.put("session.timeout.ms", "30000");
+            props.put("auto.offset.reset", "earliest");
+            props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+            props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+            this.topic = topic;
+            this.linesToFlush = linesToFlush;
+            this.output = output;
+        }
+
+        @Override
+        public void execute() throws Exception {
+
+            //@SuppressWarnings("resource")
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(this.output, true))) {
+
+                //long diff = 0;
+                int buffer = 0;
+                @SuppressWarnings("resource")
+                KafkaConsumer<String, String> consumer = new KafkaConsumer<>(this.props);
+                consumer.subscribe(Collections.singletonList(this.topic));
+                while (true) {
+
+                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                    for (ConsumerRecord<String, String> record : records) {
+
+                        //diff = record.timestamp() - Long.parseLong(record.value().split(" ")[1]);
+                        //bw.append(record.value() + " " + record.timestamp() + " " + diff + "\n");
+                        bw.append(record.value());
+                        buffer++;
+                        if (buffer == this.linesToFlush) {
+
+                            buffer = 0;
+                            bw.flush();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
--- workload_manager/src/main/java/de/tu_berlin/dos/arm/khaos/workload_manager/Run.java
+++ workload_manager/src/main/java/de/tu_berlin/dos/arm/khaos/workload_manager/Run.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.tu_berlin.dos.arm.khaos.common.data.Event;
 import de.tu_berlin.dos.arm.khaos.common.utils.FileReader;
+import de.tu_berlin.dos.arm.khaos.workload_manager.Consumers.Consumer;
+import de.tu_berlin.dos.arm.khaos.workload_manager.Consumers.KafkaToFileConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.common.serialization.Serializer;
 import org.apache.log4j.Logger;
@@ -16,53 +18,18 @@ public class Run {
 
     private static final Logger LOG = Logger.getLogger(Run.class);
 
-    public static class EventSerializer implements Serializer<Event> {
-
-        private static final Logger LOG = Logger.getLogger(EventSerializer.class);
-
-        private final ObjectMapper objectMap = new ObjectMapper();
-
-        @Override
-        public void configure(Map<String, ?> configs, boolean isKey) { }
-
-        @Override
-        public byte[] serialize(String topic, Event event) {
-            try {
-                String msg = objectMap.writeValueAsString(event);
-                return msg.getBytes();
-            }
-            catch(JsonProcessingException ex) {
-                LOG.error("Error in Serialization", ex);
-            }
-            return null;
-        }
-
-        @Override
-        public void close() { }
-    }
-
     public static void main(String[] args) throws Exception {
 
         // get properties file
         Properties generatorProps = FileReader.GET.read("generator.properties", Properties.class);
 
-        // retrieve dataset file
-        String fileName = generatorProps.getProperty("dataset.fileName");
-        File file = FileReader.GET.read(fileName, File.class);
+        // retrieve file for outputting events
+        String brokerList = generatorProps.getProperty("kafka.brokerList");
+        String topic = generatorProps.getProperty("kafka.topic");
+        String fileName = generatorProps.getProperty("dataset.outputFile");
+        File outputFile = FileReader.GET.read(fileName, File.class);
 
-        // configure kafka producer
-        Properties kafkaProps = new Properties();
-        kafkaProps.put("bootstrap.servers", generatorProps.getProperty("kafka.brokerList"));
-        kafkaProps.put("acks", "0");
-        kafkaProps.put("retries", 0);
-        kafkaProps.put("batch.size", 16384);
-        kafkaProps.put("linger.ms", 1000);
-        kafkaProps.put("buffer.memory", 33554432);
-        kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        kafkaProps.put("value.serializer", EventSerializer.class.getName());
-
-        KafkaProducer<String, Event> kafkaProducer = new KafkaProducer<>(kafkaProps);
-
-
+        Consumer consumer = new KafkaToFileConsumer(brokerList, topic, 100000, outputFile);
+        consumer.execute();
     }
 }
