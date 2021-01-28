eb37c7e1c00a6683c466835bad24294384a86386
Morgan Karl Geldenhuys <morgan.geldenhuys@tu-berlin.de>
2021-01-28 16:46:50 +0100

    set to latest

--- workload_manager/src/main/java/de/tu_berlin/dos/arm/khaos/workload_manager/Consumers.java
+++ workload_manager/src/main/java/de/tu_berlin/dos/arm/khaos/workload_manager/Consumers.java
@@ -35,7 +35,7 @@ public interface Consumers {
             props.put("enable.auto.commit", "true");
             props.put("auto.commit.interval.ms", "1000");
             props.put("session.timeout.ms", "30000");
-            props.put("auto.offset.reset", "earliest");
+            props.put("auto.offset.reset", "latest");
             props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
             props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
