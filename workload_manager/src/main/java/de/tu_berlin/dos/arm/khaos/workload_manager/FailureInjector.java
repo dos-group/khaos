f5a3c32d6c973345e50c1d7a6da2a5e25f36420b
pfisterb <>
2021-03-03 13:15:54 +0100

    Refactor

--- workload_manager/src/main/java/de/tu_berlin/dos/arm/khaos/workload_manager/FailureInjector.java
+++ workload_manager/src/main/java/de/tu_berlin/dos/arm/khaos/workload_manager/FailureInjector.java
@@ -1,62 +1,101 @@
 package de.tu_berlin.dos.arm.khaos.workload_manager;
 
-import de.tu_berlin.dos.arm.khaos.common.utils.FileReader;
-import io.fabric8.kubernetes.api.model.PodList;
-import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClientException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.*;
+import io.fabric8.kubernetes.client.dsl.ExecListener;
+import io.fabric8.kubernetes.client.dsl.ExecWatch;
+import io.fabric8.kubernetes.client.utils.HttpClientUtils;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
 
-import java.util.Properties;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class FailureInjector {
 
-    private static final Logger logger = LoggerFactory.getLogger(FailureInjector.class);
+    private final OkHttpClient okHttpClient;
+    private final KubernetesClient client;
 
-    public static void crashFailure(String targetLabel, String namespace, int targetCount) throws InterruptedException, ExecutionException, TimeoutException {
+    public FailureInjector() {
+        Config config = new ConfigBuilder().build();
+        this.okHttpClient = HttpClientUtils.createHttpClient(config);
+        this.client = new DefaultKubernetesClient(okHttpClient, config);
+    }
 
-        // connect to k8s with $HOME/.kube/config file
-        final ConfigBuilder configBuilder = new ConfigBuilder();
-        try (KubernetesClient client = new DefaultKubernetesClient(configBuilder.build())) {
+    public String execCommandOnPod(String podName, String namespace, String... cmd) throws InterruptedException, ExecutionException, TimeoutException {
+        Pod pod = client.pods().inNamespace(namespace).withName(podName).get();
+        System.out.printf("Running command: [%s] on pod [%s] in namespace [%s]%n",
+                Arrays.toString(cmd), pod.getMetadata().getName(), namespace);
 
-            // collect list of pods to target
-            PodList podList =  client.pods().inNamespace(namespace).withLabel(targetLabel).list();
-
-            // inject target number of pods with failure
-            for (int i=0; i<targetCount; i++) {
-
-                String podName = podList.getItems().get(i).getMetadata().getName();
-
-                // delete pods
-                client.pods().inNamespace(namespace).withName(podName).delete();
-
-                /*
-                // kill container
-                try (ExecuteCommandOnPod example = new ExecuteCommandOnPod()) {
-                    example.execCommandOnPod(podName, namespace, "sh", "-c", "kill 1");
-                }
-                */
-
-            }
-
-        } catch (KubernetesClientException e) {
-            logger.error(e.getMessage(), e);
+        CompletableFuture<String> data = new CompletableFuture<>();
+        try (ExecWatch execWatch = execCmd(pod, data, cmd)) {
+            return data.get(10, TimeUnit.SECONDS);
         }
+    }
+
+    private ExecWatch execCmd(Pod pod, CompletableFuture<String> data, String... command) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        return client.pods()
+                .inNamespace(pod.getMetadata().getNamespace())
+                .withName(pod.getMetadata().getName())
+                .writingOutput(baos)
+                .writingError(baos)
+                .usingListener(new Listener(data, baos))
+                .exec(command);
+    }
+
+    static class Listener implements ExecListener {
+
+        private CompletableFuture<String> data;
+        private ByteArrayOutputStream baos;
+
+        public Listener(CompletableFuture<String> data, ByteArrayOutputStream baos) {
+            this.data = data;
+            this.baos = baos;
+        }
+
+        @Override
+        public void onOpen(Response response) {
+            //System.out.println("Reading data... " + response.message());
+        }
+
+        @Override
+        public void onFailure(Throwable t, Response response) {
+            //System.err.println(t.getMessage() + " " + response.message());
+            data.completeExceptionally(t);
+        }
+
+        @Override
+        public void onClose(int code, String reason) {
+            // System.out.println("Exit with: " + code + " and with reason: " + reason);
+            data.complete(baos.toString());
+        }
+    }
+
+    public void crashFailure(String podName, String namespace) throws InterruptedException, ExecutionException, TimeoutException {
+
+        // kill container
+        execCommandOnPod(podName, namespace, "sh", "-c", "kill 1");
+
+        // delete pod
+        // client.pods().inNamespace(namespace).withName(podName).delete();
+
     }
 
     public static void main(String[] args) throws Exception {
 
         final String namespace = "default";
-        final String targetLabel = "component=taskmanager";
-        final int targetCount = 1;
-
+        final String podName = "flink-native-taskmanager-1-11";
 
         // inject crash failure
-        crashFailure(targetLabel, namespace, targetCount);
+        FailureInjector failureInjector = new FailureInjector();
+        failureInjector.crashFailure(podName, namespace);
+
+        failureInjector.client.close();
 
     }
 }
